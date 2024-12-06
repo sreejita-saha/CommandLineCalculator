21e3a977883cb3bff9a0c53a3d88343201d2373a
sreejita-saha <sreejita_saha@outlook.com>
2024-12-06 11:45:50 +0000

    Update LengthConverter.java
    had to implement loops and other things for it to give an option to exit and return to the main. Also created a displayLengthConverter method to be able to display this in the main.

--- LengthConverter.java
+++ LengthConverter.java
@@ -2,55 +2,69 @@
 Just did some odd length conversions
 */
 
-import java.util.*;
+import java.util.Scanner;
+
 public class LengthConverter {
-    public static void main(String[] args) {
+
+    public static void displayLengthCalculator() {
         Scanner input = new Scanner(System.in);
         double lengthConversion = 0;
         int lengthOption = 0;
 
+        // Length Converter loop
+        while (true) {
+            System.out.println("Welcome to the Length Converter.");
+            System.out.println("The following options are: \n" +
+                    "1. Meters to Yards           2. Yards to Meters\n" +
+                    "3. Kilometers to Miles       4. Miles to Kilometers\n" +
+                    "5. Inches to Cm              6. Cm to Inches\n" +
+                    "0. Exit");
 
-        System.out.println("Welcome to the Length Converter.");
-        System.out.println("The following options are: \n" +
-                "1. Meters to Yards           2. Yards to Meters\n" +
-                "3. Kilometers to Miles       4. Miles to Kilometers\n" +
-                "5. Inches to Cm              6. Cm to Inches");
-
-        while (lengthOption < 1 || lengthOption > 6) {
             System.out.print("Select one of the following options: ");
             lengthOption = input.nextInt();
-            if (lengthOption < 1 || lengthOption > 6) {
-                System.out.println("Please select a number between 1 and 6!");
-            }
-        }
 
-        System.out.print("Enter the length chosen: ");
-        double length = input.nextDouble();
-        switch(lengthOption) {
-            case 1:
-                lengthConversion = length * 1.09361;
-                System.out.println(length + " meters is " + lengthConversion + " yards");
-                break;
-            case 2:
-                lengthConversion = length * 0.9144;
-                System.out.println(length + " yards is " + lengthConversion + " meters");
-                break;
-            case 3:
-                lengthConversion = length * 0.621371;
-                System.out.println(length + " kilometers is " + lengthConversion + " miles");
-                break;
-            case 4:
-                lengthConversion = length * 1.60934;
-                System.out.println(length + " miles is " + lengthConversion + " kilometers");
-                break;
-            case 5:
-                lengthConversion = length * 2.54;
-                System.out.println(length + " inches is " + lengthConversion + " cm");
-                break;
-            case 6:
-                lengthConversion = length/2.54;
-                System.out.println(length + " cm is " + lengthConversion + " inches");
-                break;
+            // If the user chooses 0, return to the main menu
+            if (lengthOption == 0) {
+                System.out.println("Exiting Length Converter...");
+                break;  // Exits this method, returning to the main menu
+            }
+
+            // Validate input choice
+            if (lengthOption < 0 || lengthOption > 6) {
+                System.out.println("Please select a number between 0 and 6!");
+                continue;  // Continue the loop and ask for input again
+            }
+
+            System.out.print("Enter the length chosen: ");
+            double length = input.nextDouble();
+
+            // Perform the conversion based on the user's choice
+            switch (lengthOption) {
+                case 1: // Meters to Yards
+                    lengthConversion = length * 1.09361;
+                    System.out.println(length + " meters is " + lengthConversion + " yards");
+                    break;
+                case 2: // Yards to Meters
+                    lengthConversion = length * 0.9144;
+                    System.out.println(length + " yards is " + lengthConversion + " meters");
+                    break;
+                case 3: // Kilometers to Miles
+                    lengthConversion = length * 0.621371;
+                    System.out.println(length + " kilometers is " + lengthConversion + " miles");
+                    break;
+                case 4: // Miles to Kilometers
+                    lengthConversion = length * 1.60934;
+                    System.out.println(length + " miles is " + lengthConversion + " kilometers");
+                    break;
+                case 5: // Inches to Centimeters
+                    lengthConversion = length * 2.54;
+                    System.out.println(length + " inches is " + lengthConversion + " cm");
+                    break;
+                case 6: // Centimeters to Inches
+                    lengthConversion = length / 2.54;
+                    System.out.println(length + " cm is " + lengthConversion + " inches");
+                    break;
+            }
         }
     }
 }
