8474b2eacabdcb00b24e7a2ee333c75a6a64176d
sreejita-saha <sreejita_saha@outlook.com>
2024-12-16 13:38:39 +0000

    calculator

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Calculator {
-    public static void main(String[] args) {
+    public static void displayCalculator() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the Calculator!");
 
