a0464813213f143b2bd4c3c4011d8749c24f3350
sreejita-saha <sreejita_saha@outlook.com>
2024-12-16 12:48:14 +0000

    Update LengthConverter.java
    OOP design

--- LengthConverter.java
+++ LengthConverter.java
@@ -1,67 +1,60 @@
-import java.util.*;
+import java.util.Scanner;
 
-public class LengthConverter {
+public class LengthConverter extends BaseConverter {
+    private final Scanner input = new Scanner(System.in);  // Initialize the scanner
+    private final LengthConversions lengthConversions = new LengthConversions();  // Create an instance of LengthConversions
 
-    public static void displayLengthCalculator() {
-        LengthConversions lengthConversions = new LengthConversions(); // creating object
-        Scanner input = new Scanner(System.in);
+    @Override
+    public void displayMenu() {
+        System.out.println("Welcome to the Length Converter.");
+        System.out.println("Select one of the following options: \n" +
+                "1. Meters to Yards   2. Yards to Meters\n" +
+                "3. Kilometers to Miles   4. Miles to Kilometers\n" +
+                "5. Inches to Centimeters   6. Centimeters to Inches\n" +
+                "0. Exit");
+    }
+
+    @Override
+    public void performConversion(int lengthOption) {
+        System.out.print("Enter the length: ");
+        double length = input.nextDouble();
         double lengthConversion = 0;
-        int lengthOption = 0;
 
-        // Length Converter loop
-        while (true) {
-            System.out.println("Welcome to the Length Converter.");
-            System.out.println("The following options are: \n" +
-                    "1. Meters to Yards           2. Yards to Meters\n" +
-                    "3. Kilometers to Miles       4. Miles to Kilometers\n" +
-                    "5. Inches to Cm              6. Cm to Inches\n" +
-                    "0. Exit");
+        if (lengthOption < 0 || lengthOption > 6) {
+            System.out.println("Please select a number between 0 and 6!");
+            return;  // Exit the method if the option is invalid
+        }
 
-            System.out.print("Select one of the following options: ");
-            lengthOption = input.nextInt();
-
-            // If the user chooses 0, return to the main menu
-            if (lengthOption == 0) {
-                System.out.println("Exiting Length Converter...");
-                break;  // Exits this method, returning to the main menu
-            }
-
-            // Validate input choice
-            if (lengthOption < 0 || lengthOption > 6) {
-                System.out.println("Please select a number between 0 and 6!");
-                continue;  // Continue the loop and ask for input again
-            }
-
-            System.out.print("Enter the length chosen: ");
-            double length = input.nextDouble();
-
-            // Perform the conversion based on the user's choice
-            switch (lengthOption) {
-                case 1: // Meters to Yards
-                    lengthConversion = lengthConversions.metersToYards(length);
-                    System.out.println(length + " meters is " + lengthConversion + " yards");
-                    break;
-                case 2: // Yards to Meters
-                    lengthConversion = lengthConversions.yardsToMeters(length);
-                    System.out.println(length + " yards is " + lengthConversion + " meters");
-                    break;
-                case 3: // Kilometers to Miles
-                    lengthConversion = lengthConversions.kilometersToMiles(length);
-                    System.out.println(length + " kilometers is " + lengthConversion + " miles");
-                    break;
-                case 4: // Miles to Kilometers
-                    lengthConversion = lengthConversions.milesToKilometers(length);
-                    System.out.println(length + " miles is " + lengthConversion + " kilometers");
-                    break;
-                case 5: // Inches to Centimeters
-                    lengthConversion = lengthConversions.inchesToCentimeters(length);
-                    System.out.println(length + " inches is " + lengthConversion + " cm");
-                    break;
-                case 6: // Centimeters to Inches
-                    lengthConversion = lengthConversions.centimetersToInches(length);
-                    System.out.println(length + " cm is " + lengthConversion + " inches");
-                    break;
-            }
+        switch (lengthOption) {
+            case 1: // Meters to Yards
+                lengthConversion = lengthConversions.metersToYards(length);
+                System.out.println(length + " meters is " + lengthConversion + " yards");
+                break;
+            case 2: // Yards to Meters
+                lengthConversion = lengthConversions.yardsToMeters(length);
+                System.out.println(length + " yards is " + lengthConversion + " meters");
+                break;
+            case 3: // Kilometers to Miles
+                lengthConversion = lengthConversions.kilometersToMiles(length);
+                System.out.println(length + " kilometers is " + lengthConversion + " miles");
+                break;
+            case 4: // Miles to Kilometers
+                lengthConversion = lengthConversions.milesToKilometers(length);
+                System.out.println(length + " miles is " + lengthConversion + " kilometers");
+                break;
+            case 5: // Inches to Centimeters
+                lengthConversion = lengthConversions.inchesToCentimeters(length);
+                System.out.println(length + " inches is " + lengthConversion + " cm");
+                break;
+            case 6: // Centimeters to Inches
+                lengthConversion = lengthConversions.centimetersToInches(length);
+                System.out.println(length + " cm is " + lengthConversion + " inches");
+                break;
+            case 0: // Exit
+                System.out.println("Exiting the program.");
+                break;
+            default:
+                System.out.println("Invalid option selected.");
         }
     }
 }
